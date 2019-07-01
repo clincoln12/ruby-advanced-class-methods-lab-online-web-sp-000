b6bcfbd8096dbd24a9eaec7908839cf9443bfa64
Chris Lincoln <chris.lincoln12@gmail.com>
2019-07-01 20:23:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -48,6 +48,7 @@ class Song
   def self.new_from_filename(name)
     song = self.new 
     song.name = name.split(" - ")[1].gsub(".mp3", "")
+    song.artist_name = name.split(" - ")[0]
     song
   end
 
